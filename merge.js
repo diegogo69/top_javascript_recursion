0214a2436f2d0ffe4a07c0ad7066cf72c0ef33cf
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2025-01-15 17:14:45 -0400

    REFAC: Correct comments

--- merge.js
+++ merge.js
@@ -1,45 +1,39 @@
-// Merge sort algorithm implementation
-function ms(arr) {
+// Merge sort algorithm implementation using js built in methods
+function mergeSort(arr) {
   // Return 1 item long array. (Technically sorted)
   if (arr.length === 1) return arr;
 
   // If array more than 1 item long, divide and sort each half
-
-  // Get 'middle' element of array
+  // Get 'middle' element of array and sort each half
+  // First half (a). Second half (b)
   let m = Math.ceil(arr.length / 2);
+  let a = mergeSort(arr.slice(0, m));
+  let b = mergeSort(arr.slice(m));
 
-  // Sort first half
-  let a = ms(arr.slice(0, m));
-  // Sort second half
-  let b = ms(arr.slice(m));
-
-  // Merged two sorted halves
+  // Array that stores merged sorted items
   let merged = [];
 
   // While there's at least 1 item in both arrays
   while (a[0] && b[0]) {
-    // Less than comparison. Sort lesser to greater
+    // Less than comparison for lesser to greater sorting
+    // If first item is lesser, add to merged and delete from half
     if (a[0] < b[0]) {
-      // If first item of 'a' half lesser, push to merged
       merged.push(a[0]);
-      // Delete first 'a' half first item after it was added
       a.splice(0, 1);
     } else {
-      // Push first 'b' half item if lesser
       merged.push(b[0]);
-      // Delete after added
       b.splice(0, 1);
     }
   }
 
-  // If half has no more items, add all of the other half
+  // If half has no more items, add all item's of the other half
   if (!a[0]) merged.push(...b);
   else if (!b[0]) merged.push(...a);
 
+  // Return merged sorted list of items
   return merged;
 }
 
-const a = ms([3, 2, 1, 13, 8, 5, 0, 1]);
-console.log(a);
-
-// [0, 1, 1, 2, 3, 5, 8, 13]
+const unsorted = [3, 2, 1, 13, 8, 5, 0, 1];
+const sorted = mergeSort(unsorted);
+console.log(sorted);
